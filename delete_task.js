84be9d5ab9ad9acd3d1f4d961c4c3cf4d2e286f1
Kunal <kunaltyagi760@gmail.com>
2024-06-04 19:56:18 +0530

    fuctionality completed

--- delete_task.js
+++ delete_task.js
@@ -2,6 +2,7 @@ const createTask = document.getElementById("btn1")
 let arr = JSON.parse(localStorage.getItem("tasks")) || []
 let deletedTasks = JSON.parse(localStorage.getItem("deletedTasks")) || []
 
+
 const displayRow = (arr) => {
     arr.forEach((el) => {
         let row = document.createElement("tr")
@@ -39,6 +40,32 @@ const restore = (taskId) => {
     location.reload();
 }
 
+const filterPriority = () => {
+    let priority = document.getElementById("filterByPriority").value
+    let tbody = document.querySelector("tbody");
+    tbody.innerHTML = null;
 
+    if (priority === ""){
+        displayRow(deletedTasks);
+    } else{
+        let filterTask = deletedTasks.filter((item) => item.priority === priority)
+
+        displayRow(filterTask);
+    }
+}
+
+const filterStatus = () => {
+    let status = document.getElementById("filterByStatus").value
+    let tbody = document.querySelector("tbody");
+    tbody.innerHTML = null;
+
+    if (status === ""){
+        displayRow(deletedTasks);
+    } else{
+        let filterTask = deletedTasks.filter((item) => item.status === status)
+
+        displayRow(filterTask);
+    }
+}
 
 displayRow(deletedTasks);
